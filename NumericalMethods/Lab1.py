624fc63079db8e1225f37045aebe564bf7643218
Oleh <olehtomas@gmail.com>
2017-09-23 17:59:29 +0300

    fix

--- NumericalMethods/Lab1.py
+++ NumericalMethods/Lab1.py
@@ -45,12 +45,13 @@ def u_derivative_2(x):
 
 # left part
 def f(x):
-	return k1*k2* m.sin(k2*x) * u_derivative_1(x) 
-	+ k(x) * u_derivative_2(x) + p(x) * u_derivative_1(x) + q(x) * u(x)
+    return - der1_k(x) * u_derivative_1(x) - k(x) * u_derivative_2(x) + p(x) * u_derivative_1(x) + q(x) * u(x)
 
 myu1 = - k(a)*u_derivative_1(a) + alfa1 * u(a)
 
 myu2 = k(b)*u_derivative_1(b) + alfa2 * u(b)
+
+print("mu: ", myu1, myu2)
 
 # зведення задачі до однорідних умов v = u - psi
 # psi = A*x + B
@@ -59,96 +60,90 @@ A = (alfa2 * myu1 - alfa1 * myu2) / (alfa2*(-k(a)+a*alfa1) - alfa1*(k(b)+b*alfa2
 B = (myu2 - A*(k(b) + b * alfa2)) / alfa2
 
 def psi(x):
-	return A * x + B
+    return A * x + B
 
 def v(x):
-	return u(x) - psi(x)
+    return u(x) - psi(x)
 
 _A = k(b) * (b-a) / (2*k(b) + alfa2*(b-a)) + b
 _B = a - k(a) * (b-a) / (2*k(a) + alfa1*(b-a))
 
 def phi_i(i, x):
-	if i == 1:
-		return m.pow(x - a, 2) * (x - _A)
-	if i == 2:
-		return m.pow(b - x, 2) * (_B - x)
-	return m.pow(x - a, 2) * m.pow(b - x, i-1)
+    if i == 1:
+        return m.pow(x - a, 2) * (x - _A)
+    if i == 2:
+        return m.pow(b - x, 2) * (_B - x)
+    return m.pow(x - a, 2) * m.pow(b - x, i-1)
 
 def der1_phi_i(i, x):
-	if i == 1:
-		return m.pow(x - a, 2) + 2 * (x - a) * (x - _A)
-	if i == 2:
-		return - m.pow(b - x, 2) - 2 * (b - x) * (_B - x)
-	return 2 * (x - a) * m.pow(b - x, i - 1) - (i - 1) * m.pow(x - a, 2) * m.pow(b - x, i - 2) 
+    if i == 1:
+        return m.pow(x - a, 2) + 2 * (x - a) * (x - _A)
+    if i == 2:
+        return - m.pow(b - x, 2) - 2 * (b - x) * (_B - x)
+    return 2 * (x - a) * m.pow(b - x, i - 1) - (i - 1) * m.pow(x - a, 2) * m.pow(b - x, i - 2)
 
 def der2_phi_i(i, x):
-	if i == 1:
-		return 2 * (x - a) + 2 * (x - _A) + 2 * (x - a)
-	if i == 2:
-		return 2 * (b - x) + 2 * (b - x) + 2 * (_B - x)
-	return 2 * m.pow(b-x, i-1) - 2*(i-1)*(x-a)*m.pow(b-x,i-2) + (i-1)*(i-2)*(x-a)*(x-a)*m.pow(b-x,i-3) - 2*(i-1)*(x-a)*m.pow(b-x, i-2)
+    if i == 1:
+        return 2 * (x - a) + 2 * (x - _A) + 2 * (x - a)
+    if i == 2:
+        return 2 * (b - x) + 2 * (b - x) + 2 * (_B - x)
+    return 2 * m.pow(b-x, i-1) - 2*(i-1)*(x-a)*m.pow(b-x,i-2) + (i-1)*(i-2)*(x-a)*(x-a)*m.pow(b-x,i-3) - 2*(i-1)*(x-a)*m.pow(b-x, i-2)
 
 # Au = (-k(x)*u`)` + p(x)*u` + q(x)*u
 
 def A_phi(phi_i, i, x):
+    return - der1_k(x)*der1_phi_i(i,x) \
+           - k(x)*der2_phi_i(i,x) \
+           + p(x)*der1_phi_i(i,x) \
+           + q(x)*phi_i(i, x)
 
-	return - der1_k(x)*der1_phi_i(i,x) \
-	- k(x)*der2_phi_i(i,x) \
-	+ p(x)*der1_phi_i(i,x) \
-	+ q(x)*phi_i(i, x) 
+def Au(u, x):
+    return - der1_k(x)*u_derivative_1(x) - k(x)*u_derivative_2(x) + p(x)*u_derivative_1(x) + q(x)*u(x)
 
-def Au(u, x) :
-	return - der1_k(x)*u_derivative_1(x) - k(x)*u_derivative_2(x) + p(x)*u_derivative_1(x) + q(x)*u(x)
+v_derivative_1 = lambda x: u_derivative_1(x) - A
+v_derivative_2 = lambda x: u_derivative_2(x)
+
+def Av(v, x):
+    return - der1_k(x)*v_derivative_1(x) - k(x)*v_derivative_2(x) + p(x)*v_derivative_1(x) + q(x)*v(x)
 
 # x = 3
 matrixA=[
-	[
-		(integrate.quad(lambda x: A_phi(phi_i,i+1, x) * phi_i(j+1, x), a, b))[0] 
-		for j in range(n)
-	] for i in range(n)
-]
+        [
+            (integrate.quad(lambda x: A_phi(phi_i, j + 1, x) * phi_i(i + 1, x), a, b))[0]
+            for j in range(n)
+        ] for i in range(n)
+    ]
 
 b_array=[
-	(integrate.quad(lambda x: Au(f, x) * phi_i(j+1, x), a, b))[0] for j in range(n)
-]
+    (integrate.quad(lambda x: Av(v, x) * phi_i(j+1, x), a, b))[0] for j in range(n)
+    ]
 
 def my_print(matrix):
-	for row in matrix:
-		print([round(value, 3) for value in row])
+    for row in matrix:
+        print([round(value, 3) for value in row])
 
 c = np.linalg.solve(np.array(matrixA), np.array(b_array))
 my_print(matrixA)
 print("c:", c)
 
-def product(M, v):
-	res = []
-	for row in M:
-		tmp = 0
-		for i in range(0,n):
-			tmp+=c[i]*row[i]
-		res.append(tmp)
+approximate_v = lambda x: sum([c[i] * phi_i(i + 1, x) for i in range(n)])
+approximate_u = lambda x: approximate_v(x) + psi(x)
 
-product(matrixA, b_array)
-
-def approximate_u(x):
-	res = 0
-	for i in range(0,n):
-		res += (c[i] * phi_i(i, x))
-	return res
-
-
-x_i = np.arange(a, b, 0.1)
-y_i = []
+x_i = np.arange(a, b, 0.01)
+# y_u = []
+y_v = []
 y_apr_i = []
 
 def fill_y_i():
-	for i in x_i:
-		y_i.append(u(i))
-		y_apr_i.append(approximate_u(i))
+    for i in x_i:
+        y_v.append(v(i) + psi(i))
+        # y_u.append(u(i))
+        y_apr_i.append(approximate_u(i))
 
 fill_y_i()
-plt.plot(x_i, y_i)
-plt.plot(x_i, y_apr_i)
+# plt.plot(x_i, y_u, 'r')
+plt.plot(x_i, y_v, 'g')
+plt.plot(x_i, y_apr_i, 'b')
 plt.show()
 
 res, _ = integrate.quad(u_derivative_1, 0, m.pi/4)
