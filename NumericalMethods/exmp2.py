f75b6d4be3fe733f1e80e2cb92e778d01ddea140
Dobryk15 <dobrovolska75@ukr.net>
2017-12-05 00:27:50 +0200

    ImportantFix

--- NumericalMethods/exmp2.py
+++ NumericalMethods/exmp2.py
@@ -7,24 +7,27 @@ x = Symbol('x')
 # t = Symbol('t')
 
 N = 20
-R = 1
+R = 0.01
 T = 15*60
 
 u0 = 273
 u_kr = 173
-u_env = 393
+u_env = 573
+u_finish = 563
 
-lamda = 42.1 # Bт/(м*K)
-c = 445 # Дж/(кг*К)
-ro = 7800 # кг/м^3
-gamma = 140 # Bт/(м^2*K)
+lamda = 0.175 # Bт/(м*K)
+c = 1300 # Дж/(кг*К)
+ro = 1400 # кг/м^3
+gamma = 58.2 # Bт/(м^2*K)
 
-mu2 = (u_env - u_kr) * gamma * R / u_kr / lamda
+mu2_ = (u_env - u_kr) * gamma * R / u_kr / lamda
+
+# mu2_ = 0
 
 gamma1 = gamma * R / lamda
 
 beta1, beta2 = 0, gamma1
-alpha1, alpha2 = 1,1
+alpha1, alpha2 = 1, 1
 
 def get_sigma(i):
     sigma = [0, 0.5, 1]
@@ -38,52 +41,50 @@ def t_prev(t1):
 
 h = 1/N
 tau = t1(10)
-T_ = t1(T)
-
-def mu2_(t, sigma):
-    return (sin(0.01*(t + sigma * tau)) - u_kr) * gamma * R / u_kr / lamda
-
-def mu_2(t):
-    return (sin(0.01*t1(t)) - u_kr) * gamma * R / u_kr / lamda
 
 def v0():
     return (u0 - u_kr) / u_kr
 
-x0_ = integrate(x**2, (x, 0, h))/h 
+def v_end():
+    return (u_finish - u_kr) / u_kr
 
-xN_ = integrate(x**2, (x, 1-h, 1))/h 
+x0_ = integrate(x**2, (x, 0, h)) / h 
+
+xN_ = integrate(x**2, (x, 1-h, 1)) / h 
 
 def xi_(i):
-    return integrate(x**2, (x, h*(i-1), h*(i+1)))/h/2 
+    return integrate(x**2, ( x, h * (i-1), h * (i+1) ) ) / h / 2 
 
-def pi_(i):
-    return (h * (i + 0.5))**2
+def pi(i):
+    return (h * (i - 0.5)) ** 2
 
 def bi_(sigma, i):
-    return sigma * tau * pi_(i)/h/h
+    return sigma * tau * pi(i+1) / h / h
 
 def ci_(sigma, i):
     return - xi_(i) - di_(sigma, i) - bi_(sigma, i)
 
 def di_(sigma, i):
-    return sigma * tau * pi_(i)/h/h
+    return sigma * tau * pi(i) / h / h
 
 def get_next_layer(y_prev, layer):
-    sigma = get_sigma(0)
+    
+    sigma = get_sigma(1)
+    
     matrix = [[0 for j in range(N + 1)] for i in range(N + 1)]
     
-    b0 = sigma * tau * alpha1 * pi_(0) / h / h
+    b0 = sigma * tau * alpha1 * pi(1) / h / h
     c0 = - alpha1 * x0_ / 2 - b0
-    phi0 = - alpha1*x0_*y_prev[0]/2 - (1 - sigma) * tau * alpha1 * pi_(0) * (y_prev[1] - y_prev[0]) / h / h  
+    phi0 = - alpha1 * x0_ * y_prev[0] / 2 - (1 - sigma) * tau * alpha1 * pi(1) * (y_prev[1] - y_prev[0]) / h / h  
 
-    dN = sigma * tau * alpha2 * pi_(N - 1) / h / h
+    dN = sigma * tau * alpha2 * pi(N) / h / h
     
-    cN = -sigma * tau * beta2 / h - alpha2 * xN_ / 2 - dN
+    cN = - sigma * tau * beta2 / h - alpha2 * xN_ / 2 - dN
     
-    phiN = (1 - sigma) * tau * beta2 * (1 - h) * y_prev[N] / h \
-        - tau * (1 ** 2) * mu2_(layer,sigma) / h \
+    phiN = (1 - sigma) * tau * beta2 * y_prev[N] / h \
+        - tau * mu2_ / h \
         - alpha2 * xN_ * y_prev[N] / 2 \
-        + (1 - sigma) * tau * alpha2 * pi_(N-1) * (y_prev[N] - y_prev[N - 1]) / h / h 
+        + (1 - sigma) * tau * alpha2 * pi(N) * (y_prev[N] - y_prev[N - 1]) / h / h 
 
     matrix[0][0] = c0
     matrix[0][1] = b0
@@ -92,6 +93,7 @@ def get_next_layer(y_prev, layer):
     matrix[N][N - 1] = dN
 
     for i in range(1, N):
+        
         di = di_(sigma,i)
         ci = ci_(sigma,i)
         bi = bi_(sigma,i)
@@ -105,7 +107,7 @@ def get_next_layer(y_prev, layer):
     b_array[N] = phiN
 
     for i in range(1, N):
-        b_array[i] = - xi_(i) * y_prev[i] - (1-sigma)*tau*( pi_(i)*(y_prev[i+1] - y_prev[i]) - pi_(i-1)*(y_prev[i] - y_prev[i-1]) )/h/h
+        b_array[i] = - xi_(i) * y_prev[i] - (1-sigma) * tau * ( pi(i + 1) * (y_prev[i+1] - y_prev[i]) - pi(i) * (y_prev[i] - y_prev[i-1]) ) / h / h
     c = np.linalg.solve(np.array(matrix, float), np.array(b_array, float))
     
     return c
@@ -113,21 +115,27 @@ def get_next_layer(y_prev, layer):
 def draw(u_approximation_points):
     a = 0
     b = 1 
-    xs = [(a + i * h) * R for i in range(N + 1)]
+    xs = [(a + i * h) for i in range(N + 1)]
     p1, = plt.plot(xs, u_approximation_points,color='blue',label='appr_u(x)',linestyle='-.')
     plt.legend(handles=[p1], loc=1)
     
     plt.show()
 
 def main():
-    print(T_)
     yi_cur = [v0() for i in range(N + 1)]
+    u_end = v_end()
     j = tau
-    while j <= T_:
+    while yi_cur[0] < u_end:
+        print(j)
         print(yi_cur)
         yi_cur = get_next_layer(yi_cur, j)
         j = j + tau
         
+    # for i in range(0,len(yi_cur)):
+        # yi_cur[i] = (yi_cur[i] + 1) * u_kr
+    # print("tau", tau)
+    print( t_prev(j))
+    print(yi_cur)
     for i in range(0,len(yi_cur)):
         yi_cur[i] = (yi_cur[i] + 1) * u_kr
     print(yi_cur)
